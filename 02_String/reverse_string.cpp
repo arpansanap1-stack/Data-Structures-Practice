f9df2f59c284d8a386441e4f3e41e0c22e1f3771
Arpan Sanap <arpansanap1@gmail.com>
2026-08-11 23:38:31 +0530

    Created new file string

---
+++ 02_String/reverse_string.cpp
@@ -0,0 +1,23 @@
+// leetcode question: 344. Reverse String
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        
+        while (left < right) {
+            // Swap the characters using the built-in swap function
+            swap(s[left], s[right]);
+            
+            // Move pointers towards the middle
+            left++;
+            right--;
+        }
+    }
+};
+// this program runs with time compleexity of o(n) and space complexity of o(1)
